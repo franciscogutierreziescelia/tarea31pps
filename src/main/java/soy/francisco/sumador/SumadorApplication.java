4fe7e294025c36a51b9e6746ea088e4a37b47cc2
franciscogutierreziescelia <franciscogutierrez@iescelia.org>
2022-04-07 07:59:35 +0000

    séptimo commit: Modificanado SumadorApplication.java(quitando tabulaciones para que pase el test)

--- src/main/java/soy/francisco/sumador/SumadorApplication.java
+++ src/main/java/soy/francisco/sumador/SumadorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SumadorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SumadorApplication.class, args);
-	}
+ public static void main(String[] args) {
+  SpringApplication.run(SumadorApplication.class, args);
+ }
 
 }
